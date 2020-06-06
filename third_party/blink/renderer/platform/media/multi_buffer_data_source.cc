b0326004d24fe320419790df5df0c29cc4071d0b
Paul Frazee <pfrazee@gmail.com>
2020-06-06 10:30:45 -0500

    feat: add streaming-protocol registry to multibuffer_data_source
    blink::WebMediaPlayerImpl - which provides the <video> and <audio> behaviors - needs to know
    whether a data source will stream or fully buffer the response. It determines this behavior
    with MultibufferDataSource::AssumeFullyBuffered() which has http/s hardwired. An incorrect
    determination will cause the video/audio to fail playing.
    
    This patch adds a list of "streaming protocols" to the MultibufferDataSource in order to allow
    other protocols to register their streaming behavior. MultibufferDataSource::AssumeFullyBuffered()
    then refers to the list so that it can correctly determine the data source's settings.
    
    Patch-Dir: src/electron/patches/chromium
    Patch-Filename: feat_add_streaming-protocol_registry_to_multibuffer_data_source.patch

--- third_party/blink/renderer/platform/media/multi_buffer_data_source.cc
+++ third_party/blink/renderer/platform/media/multi_buffer_data_source.cc
@@ -13,8 +13,10 @@
 #include "base/functional/callback_helpers.h"
 #include "base/location.h"
 #include "base/memory/raw_ptr.h"
+#include "base/no_destructor.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/strings/string_util.h"
 #include "media/base/media_log.h"
 #include "net/base/net_errors.h"
 #include "third_party/blink/renderer/platform/media/buffered_data_source_host_impl.h"
@@ -63,7 +65,19 @@ const int kUpdateBufferSizeFrequency = 32;
 // How long to we delay a seek after a read?
 constexpr base::TimeDelta kSeekDelay = base::Milliseconds(20);
 
+std::vector<std::string>* GetStreamingSchemes() {
+  static base::NoDestructor<std::vector<std::string>> streaming_schemes({
+    url::kHttpsScheme,
+    url::kHttpScheme
+  });
+  return streaming_schemes.get();
+}
+
 }  // namespace
+
+void AddStreamingScheme(const char* new_scheme) {
+  GetStreamingSchemes()->push_back(new_scheme);
+}
 
 class MultiBufferDataSource::ReadOperation {
  public:
@@ -156,7 +170,14 @@ bool MultiBufferDataSource::media_has_played() const {
 
 bool MultiBufferDataSource::AssumeFullyBuffered() const {
   DCHECK(url_data_);
-  return !url_data_->url().ProtocolIsInHTTPFamily();
+
+  const std::string scheme = url_data_->url().Protocol().Ascii();
+  for (const std::string& streaming_scheme : *GetStreamingSchemes()) {
+    if (base::EqualsCaseInsensitiveASCII(scheme, streaming_scheme)) {
+      return false;
+    }
+  }
+  return true;
 }
 
 void MultiBufferDataSource::SetReader(
