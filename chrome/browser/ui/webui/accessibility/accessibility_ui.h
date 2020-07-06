0204603df2b1d1923ec76aeb1a50ac3f541f95e1
Shelley Vohr <shelley.vohr@gmail.com>
2020-07-06 13:46:06 -0700

    Adjust accessibility_ui for Electron
    This tweaks Chrome's Accessibility support at chrome://accessibility
    to make it usable from Electron by replacing use of the Profile PrefService
    with Electron's own PrefService in ElectronBrowserContext. It also removes
    usage of BrowserList and Browser as we subclass related methods and use our
    WindowList.
    
    Patch-Dir: src/electron/patches/chromium
    Patch-Filename: adjust_accessibility_ui_for_electron.patch

--- chrome/browser/ui/webui/accessibility/accessibility_ui.h
+++ chrome/browser/ui/webui/accessibility/accessibility_ui.h
@@ -30,6 +30,8 @@ class ScopedAccessibilityMode;
 class WebContents;
 }  // namespace content
 
+class ElectronAccessibilityUIMessageHandler;
+
 namespace user_prefs {
 class PrefRegistrySyncable;
 }  // namespace user_prefs
@@ -80,6 +82,8 @@ class AccessibilityUIMessageHandler : public content::WebUIMessageHandler {
   static void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry);
 
  private:
+  friend class ElectronAccessibilityUIMessageHandler;
+
   // Applies `mode` to `web_contents` for the lifetime of the accessibility
   // UI page.
   void SetAccessibilityModeForWebContents(content::WebContents* web_contents,
