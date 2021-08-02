25dc73f59677be3857fc477582236633e1afac98
Jeremy Rose <jeremya@chromium.org>
2021-08-02 15:56:56 -0700

    only create a console if logging to stderr
    This fixes an issue on Windows where, when `--enable-logging=file` is
    passed and the app was not run from the console, a console window would
    be created for each child process, despite logs being redirected to a
    file.
    
    Patch-Dir: src/electron/patches/chromium
    Patch-Filename: logging_win32_only_create_a_console_if_logging_to_stderr.patch

--- content/app/content_main.cc
+++ content/app/content_main.cc
@@ -326,16 +326,14 @@ NO_STACK_PROTECTOR int RunContentProcess(
 
 #if BUILDFLAG(IS_WIN)
     base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
-    if (command_line->HasSwitch(switches::kHeadless)) {
-      // When running in headless mode we want stdio routed however if
-      // console does not exist we should not create one.
-      base::RouteStdioToConsole(/*create_console_if_not_found*/ false);
-    } else if (command_line->HasSwitch(switches::kEnableLogging)) {
-      // Route stdio to parent console (if any) or create one, do not create a
-      // console in children if handles are being passed.
-      bool create_console = command_line->GetSwitchValueASCII(
-                                switches::kEnableLogging) != "handle";
-      base::RouteStdioToConsole(create_console);
+    // Route stdio to parent console (if any) or create one.
+    bool const log_to_stderr =
+        command_line->HasSwitch(switches::kEnableLogging) &&
+        command_line->GetSwitchValueASCII(switches::kEnableLogging) != "file" &&
+        !command_line->HasSwitch(switches::kLogFile) &&
+        command_line->GetSwitchValueASCII(switches::kEnableLogging) != "handle";
+    if (log_to_stderr) {
+      base::RouteStdioToConsole(true);
     }
 #endif
 
