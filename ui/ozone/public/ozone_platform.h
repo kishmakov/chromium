85f225215031fd7a61e30bbd7bc8729c3b4cfe3c
Marek Rusinowski <marekrusinowski@gmail.com>
2022-03-23 21:09:37 +0100

    introduce OzonePlatform::electron_can_call_x11 property
    We expose this additonal property in the OzonePlatform to be able to easily
    determine whatever we can call X11 functions without crashing the application
    at rutime. It would be best if eventually all usages of this property were
    replaced with clean ozone native implementations.
    
    Patch-Dir: src/electron/patches/chromium
    Patch-Filename: introduce_ozoneplatform_electron_can_call_x11_property.patch

--- ui/ozone/public/ozone_platform.h
+++ ui/ozone/public/ozone_platform.h
@@ -128,6 +128,10 @@ class COMPONENT_EXPORT(OZONE) OzonePlatform {
     // Linux only: determines if Skia can fall back to the X11 output device.
     bool skia_can_fall_back_to_x11 = false;
 
+    // Linux only: determines is Electron can call selected X11 functions while
+    // it migrates to pure ozone abstractions.
+    bool electron_can_call_x11 = false;
+
     // Wayland only: determines whether windows which are not top level ones
     // should be given parents explicitly.
     bool set_parent_for_non_top_level_windows = false;
