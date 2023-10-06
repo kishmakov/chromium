2d00676eebf05a8e0f2d162ca77c116c0ecbb36d
John Kleinschmidt <jkleinsc@electronjs.org>
2023-10-06 14:15:20 -0400

    feat: allow passing of ObjectTemplate to ObjectTemplateBuilder
    https://chromium-review.googlesource.com/c/chromium/src/+/4905829 removed the
    ObjectTemplateBuilder constructor which allows passing in an ObjectTemplate.
    
    Electron needs this constructor, namely for gin_helper::Constructible
    objects.
    
    Patch-Dir: src/electron/patches/chromium
    Patch-Filename: feat_allow_passing_of_objecttemplate_to_objecttemplatebuilder.patch

--- gin/object_template_builder.cc
+++ gin/object_template_builder.cc
@@ -216,6 +216,13 @@ ObjectTemplateBuilder::ObjectTemplateBuilder(v8::Isolate* isolate,
   template_->SetInternalFieldCount(kNumberOfInternalFields);
 }
 
+ObjectTemplateBuilder::ObjectTemplateBuilder(v8::Isolate* isolate,
+                                             const char* type_name,
+                                             v8::Local<v8::ObjectTemplate> tmpl)
+    : isolate_(isolate), type_name_(type_name), template_(tmpl) {
+  template_->SetInternalFieldCount(kNumberOfInternalFields);
+}
+
 ObjectTemplateBuilder::ObjectTemplateBuilder(
     const ObjectTemplateBuilder& other) = default;
 
