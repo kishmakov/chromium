f86c4e7ae287223d7df5493b8d731f45a6cc040f
Charles Kerr <charles@charleskerr.com>
2024-02-08 00:41:40 -0600

    fix: GetCursorScreenPoint() wrongly returns 0, 0
    Fixes #41143. Discussion of the issue at
    https://github.com/electron/electron/issues/41143#issuecomment-1933443163
    
    This patch should be backported to e29, upstreamed to Chromium, and then
    removed if it lands upstream.
    
    Patch-Dir: src/electron/patches/chromium
    Patch-Filename: fix_getcursorscreenpoint_wrongly_returns_0_0.patch

--- ui/events/x/events_x_utils.cc
+++ ui/events/x/events_x_utils.cc
@@ -591,6 +591,9 @@ gfx::Point EventLocationFromXEvent(const x11::Event& xev) {
 gfx::Point EventSystemLocationFromXEvent(const x11::Event& xev) {
   if (auto* crossing = xev.As<x11::CrossingEvent>())
     return gfx::Point(crossing->root_x, crossing->root_y);
+  if (auto* crossing = xev.As<x11::Input::CrossingEvent>())
+    return gfx::Point(Fp1616ToDouble(crossing->root_x),
+                      Fp1616ToDouble(crossing->root_y));
   if (auto* button = xev.As<x11::ButtonEvent>())
     return gfx::Point(button->root_x, button->root_y);
   if (auto* motion = xev.As<x11::MotionNotifyEvent>())
