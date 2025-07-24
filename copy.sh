81e62874fa676471b9a0bb80f79c6ddf7019a2b6
Kirill Shmakov <shmakir@yandex.ru>
2025-07-24 11:21:02 +0200

    IGNORE: Add copy.sh helper for development

---
+++ copy.sh
@@ -0,0 +1,24 @@
+SOURCE_DIR="$(pwd)/out/$1"
+
+if [ -d "/home/kishmakov/Repos/vscode" ]; then
+	DEST_DIR="/home/kishmakov/Repos/vscode/.build/electron"
+else
+	DEST_DIR="/home/kishmakov/Repos/vs/vscode/.build/electron"
+fi
+
+cp "$SOURCE_DIR/electron" "$DEST_DIR/code-oss"
+
+FILES=(
+	"snapshot_blob.bin"
+	"v8_context_snapshot.bin"
+	"icudtl.dat"
+	"libEGL.so"
+	"libGLESv2.so"
+	"libvk_swiftshader.so"
+	"libvulkan.so.1"
+	"libffmpeg.so"
+)
+
+for file in "${FILES[@]}"; do
+	cp "$SOURCE_DIR/$file" "$DEST_DIR/"
+done
